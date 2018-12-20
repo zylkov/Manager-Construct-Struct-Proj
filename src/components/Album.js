1bf964641e25d183c54518476b6ccae816d7f385
Pixel-Tentacles <zylkov2010@gmail.com>
2018-12-20 17:18:56 +0300

    Proection disgn gallary

---
+++ src/components/Album.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  icon: {
+    marginRight: theme.spacing.unit * 2,
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardContent: {
+    flexGrow: 1,
+  }
+});
+
+const cards = [1, 2, 3];
+
+function Album(props) {
+  const { classes } = props;
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          
+          <Typography variant="h6" color="inherit" noWrap>
+            Проектирование
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        
+
+        <div className={classNames(classes.layout, classes.cardGrid)}>
+          {/* End hero unit */}
+          <Grid container spacing={40}>
+            {cards.map(card => (
+              <Grid item key={card} sm={6} md={4} lg={3}>
+                <Card className={classes.card}>
+                  
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Название
+                    </Typography>
+                    <Typography>
+                      Здесь будет подробное описание проекта.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Открыть
+                    </Button>
+                    <Button size="small" color="primary">
+                      Редактировать
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </main>
+    </React.Fragment>
+  );
+}
+
+Album.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Album);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button'
+import Album from '../components/Album'
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <p>Content типо</p>
-        <Button variant='contained' color='primary'>
-          Кликни по мне плз
-        </Button>
-      </div>
-    );
+     <Album></Album> 
+    )
   }
 }
 
