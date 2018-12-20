324177a8532eac10552923a14128d01d461506a5
Pixel-Tentacles <zylkov2010@gmail.com>
2018-12-20 15:17:29 +0300

    without standart styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div>
+        <header >
           <p>My React app</p>
         </header>
         <p>Content типо</p>
