9c6e1c0be491d02e16731207431631d879d96ad0
Pixel-Tentacles <zylkov2010@gmail.com>
2018-12-20 15:31:47 +0300

    Commti

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header >
-          <p>My React app</p>
-        </header>
         <p>Content типо</p>
+        <Button variant='contained' color='primary'>
+          Кликни по мне плз
+        </Button>
       </div>
     );
   }
