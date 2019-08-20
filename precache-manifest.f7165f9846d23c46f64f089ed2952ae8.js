2dbd0f56b64ada65de1e46d97c31e0b370b4f0cf
zylkov <zylkov.work@gmail.com>
2019-08-20 16:13:00 +0300

    Updates

---
+++ precache-manifest.f7165f9846d23c46f64f089ed2952ae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dcd84ec97f6d30c6f77",
+    "url": "/Manager-Construct-Struct-Proj/static/js/runtime~main.8dcd84ec.js"
+  },
+  {
+    "revision": "ef1ce207e6cb56500b2c",
+    "url": "/Manager-Construct-Struct-Proj/static/js/main.ef1ce207.chunk.js"
+  },
+  {
+    "revision": "193b60cb0d55b6f7d736",
+    "url": "/Manager-Construct-Struct-Proj/static/js/1.193b60cb.chunk.js"
+  },
+  {
+    "revision": "193b60cb0d55b6f7d736",
+    "url": "/Manager-Construct-Struct-Proj/static/css/1.702c9f17.chunk.css"
+  },
+  {
+    "revision": "3eb6cee85a9f83522f48068db57e01d6",
+    "url": "/Manager-Construct-Struct-Proj/index.html"
+  }
+];
